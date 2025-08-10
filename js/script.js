0960dc3910c30bf993b206addf182fc23eb2dcb4
jhrocales <jhrocales@gmail.com>
2025-08-11 01:31:14 +0800

    3rd commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // typing animation
 
 var typed = new Typed(".typing", {
-   strings:["", "Graphics Designer", "English Teacher", "Social Media Manager", "Virtual Assistant", "Aspiring Voice Actress", "Creative Writing"],
+   strings:["", "Graphics Designer", "English Teacher", "Social Media Manager", "Virtual Assistant", "Aspiring Voice Actress", "Creative Writer"],
    typeSpeed: 80,
    BackSpeed:60,
    loop:true
